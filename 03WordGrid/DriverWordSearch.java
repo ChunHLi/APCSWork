9242e0550ec97e5c50e67ff1d1f50547e1efdfec
Shawn Li <shawnli050298@gmail.com>
2014-12-02 19:26:30 -0500

    Final product, couldn't do the parameters for Driver start up thing :(

--- 03WordGrid/DriverWordSearch.java
+++ 03WordGrid/DriverWordSearch.java
@@ -6,7 +6,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 public class DriverWordSearch{
     public static void main(String[]args){
-	WordGrid A = new WordGrid(20,20);
+	WordGrid A = new WordGrid(30,30);
 	A.setWordSearch();
 	System.out.println(A.WordBank());
 	A.randomize();
