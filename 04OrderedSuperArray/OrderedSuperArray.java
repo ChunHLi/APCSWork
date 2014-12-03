09e3650d0b89431808aaebb672fe5b2a7ea5cfb7
Shawn Li <shawnli050298@gmail.com>
2014-12-02 19:28:14 -0500

    Forgot the edits.

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,47 +1,48 @@
 public class OrderedSuperArray extends SuperArray{
-    public static void main(String[]args){
-    }
     public void mySort(){
 	int counter = 1;
 	String placeHolder = superArray[0];
 	int index = 0;
-	String[] newSortedSuperArray = new String[0];
-	while (superArray.length != 0){
-	    while (counter < superArray.length){
-		if (placeHolder.compareTo(superArray[counter]) > 0){
+	String[] copy = superArray.clone();
+        clear();
+	System.out.println(copy.toString());
+	System.out.println(superArray.toString());
+	while (copy.length != 0){
+	    while (counter < copy.length){
+		if (placeHolder.compareTo(copy[counter]) > 0){
 		    index = counter;
 		}
 		counter += 1;
 	    }
 	    counter = 0;
-	    placeHolder = superArray[0];
-	    newSortedSuperArray.add(superArray.remove(index));
+	    placeHolder = copy[0];
+	    add(remove(index));
 	}
-        superArray = newSortedSuperArray;
+        superArray = copy;
     }        
     public void add(String e){
-	String[] newSuperArray = new String[0];
 	int counter = 0;
-	while (e.compareTo(superArray[0]) > 0){
-	    newSuperArray.super.add(superArray.remove(0));
+	if (superArray.length == 0){
+	    super.add(e);
 	}
-	newSuperArray.super.add(e);
-	while (counter < superArray.length){
-	    newSuperArray.super.add(superArray.remove(0));
-	    counter += 1;
+	else{
+	    while (e.compareTo(superArray[counter]) > 0){
+		counter += 1;
+		if (counter == superArray.length){
+		    break;
+		}
+	    }
+	    super.add(counter,e);
 	}
-	superArray.resize(newSuperArray.length);
-	superArray = newSuperArray;
     }
-    
-    public void set(int index, String e){
+    public String set(int index, String e){
 	if (index < 0 || index >= size()){
 	    throw new IndexOutOfBoundsException();
 	}
-	String object = superArray[index];
+	String string = superArray[index];
 	superArray[index] = e;
-	superArray.mySort();
-	return object;
+	mySort();
+	return string;
     }
 }
     
