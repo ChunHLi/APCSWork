4ed5f13ff46df2e9c94567cf7e01eaa560427c91
Shawn Li <shawnli050298@gmail.com>
2014-12-06 10:27:04 -0500

    Finally completed

--- 03WordGrid/DriverWordSearch.java
+++ 03WordGrid/DriverWordSearch.java
@@ -6,10 +6,36 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 public class DriverWordSearch{
     public static void main(String[]args){
-	WordGrid A = new WordGrid(30,30);
-	A.setWordSearch();
-	System.out.println(A.WordBank());
-	A.randomize();
-        System.out.println(A.toString());
+	set(args);
     }
+    public static WordGrid start(int row, int col, long seed){
+	WordGrid Grid = new WordGrid(row, col, seed);
+	return Grid;
+    }
+    public static void set(String[]args){
+	if (args.length == 0 || args.length == 1 || args.length == 2){
+	    System.out.println("Please input at least three integer arguments");
+	}
+	if (args.length == 4 || args.length == 3){
+	    int row = Integer.parseInt(args[0]);
+	    int col = Integer.parseInt(args[1]);
+	    Long seed = Long.parseLong(args[2]);
+	    if (args.length == 4 && args[3].equals("1")){
+		    WordGrid Grid = start(row,col,seed);
+		    Grid.setWordSearch();
+		    System.out.println(Grid.WordBank());
+		    System.out.println(Grid.toString());
+	    }
+	    else{
+		WordGrid Grid = start(row, col, seed);
+		Grid.setWordSearch();
+		System.out.println(Grid.WordBank());
+		Grid.randomize();
+		System.out.println(Grid.toString());
+	    }
+	}
+	else{
+	    System.out.println("You may have inputted an incorrect argument, please try again");
+	}
+    }				        
 }
